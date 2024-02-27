9d790dcc1fb1fb85cb1ecf8ed06551cf2be6c5d5
Simonhunor5 <simon.hunor@student.ms.sapientia.ro>
2024-02-27 10:57:19 +0200

    Repository Created
    Mock Test Running

--- calculator.py
+++ calculator.py
@@ -9,3 +9,6 @@ def multiply(a, b):
  
 def substract(a, b):
     return a - b
+
+def divide(a, b):
+    return a / b
--- calculator_test.py
+++ calculator_test.py
@@ -14,3 +14,10 @@ def test_calc_substraction():
 def test_calc_multiply():
     output = calculator.multiply(2, 3)
     assert output == 6
+
+def test_calc_divide():
+    output = calculator.divide(6,3)
+    assert output == 2
+
+    output1 = calculator.divide(5,2)
+    assert output1 == 2.5
